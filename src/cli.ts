2eea7ddfa1fa23a3b8284cb7548c963e0828d638
mrbandler <mrbandler@fivefingergames.com>
2019-11-22 23:58:10 +0100

    fix: Fixed build script to compile into the correct directory

--- src/cli.ts
+++ src/cli.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
-import * as pkg from '../package.json';
+import * as pkg from './package.json';
 
 import SetCommand, { ISetCommandArgs } from './commands/set.command';
 
