7672d08860799c62c94e66a9a544d120d0efc1ca
mrbandler <mrbandler@fivefingergames.com>
2020-09-12 20:02:20 +0200

    chore(deps): Performed a dependencie upgrade and fixed commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,8 +1,6 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 module.exports = {
     extends: ['@commitlint/config-conventional'],
-    parserPreset: 'conventional-changelog-atom',
-    formatter: '@commitlint/format',
     rules: {
         'subject-case': [2, 'always', 'sentence-case'],
         'type-enum': [
